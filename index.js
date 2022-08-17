e4e3b5c0f5769f4dff51a0f02609d7feeab4b650
lemuelnjuki <lemuel.njuki@student.moringaschool.com>
2022-08-17 13:08:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+function mondayWork(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+}
+function wrapAdjective (star = '*'){
+  const adjective = function(adjective='a hard worker'){
+      return `You are ${star}${adjective}${star}!`
+   }
+   return adjective
+}
+function wrapAdjective (or ='||'){
+    const coding =function(adjective='a dedicated programmer'){
+        return `You are ${or}${adjective}${or}!`
+    }
+    return coding
+}
+
